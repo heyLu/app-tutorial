506d35839d0212423e66930e3592ef747e58fde2
Brenton Ashworth <brenton@thinkrelevance.com>
2013-05-30 21:47:56 -0400

    use transform-enable to allow user actions to increment the counter

--- tutorial-client/app/src/tutorial_client/behavior.clj
+++ tutorial-client/app/src/tutorial_client/behavior.clj
@@ -1,10 +1,15 @@
 (ns ^:shared tutorial-client.behavior
     (:require [clojure.string :as string]
+              [io.pedestal.app :as app]
               [io.pedestal.app.messages :as msg]))
 
 (defn inc-transform [old-value _]
   ((fnil inc 0) old-value))
 
+(defn init-emitter [_]
+  [[:transform-enable [:tutorial :my-counter] :inc [{msg/topic [:my-counter]}]]])
+
 (def example-app
   {:version 2
-   :transform [[:inc [:my-counter] inc-transform]]})
+   :transform [[:inc [:my-counter] inc-transform]]
+   :emit [{:in #{[:*]} :fn (app/default-emitter :tutorial) :init init-emitter}]})
