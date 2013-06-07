9b26ffc1c5f2562d14d9387a13b475cda2ff9858
Brenton Ashworth <brenton@thinkrelevance.com>
2013-06-07 11:49:16 -0400

    simulate service input

--- tutorial-client/app/src/tutorial_client/behavior.clj
+++ tutorial-client/app/src/tutorial_client/behavior.clj
@@ -6,10 +6,17 @@
 (defn inc-transform [old-value _]
   ((fnil inc 0) old-value))
 
+(defn swap-transform [_ message]
+  (:value message))
+
 (defn init-emitter [_]
   [[:transform-enable [:tutorial :my-counter] :inc [{msg/topic [:my-counter]}]]])
 
 (def example-app
   {:version 2
-   :transform [[:inc [:my-counter] inc-transform]]
-   :emit [{:in #{[:*]} :fn (app/default-emitter :tutorial) :init init-emitter}]})
+   :transform [[:inc [:my-counter] inc-transform]
+               [:swap [:other-counters :*] swap-transform]]
+   :emit [{:in #{[:my-counter]
+                 [:other-counters :*]}
+           :fn (app/default-emitter :tutorial)
+           :init init-emitter}]})
--- tutorial-client/app/src/tutorial_client/simulated/start.cljs
+++ tutorial-client/app/src/tutorial_client/simulated/start.cljs
@@ -1,9 +1,15 @@
 (ns tutorial-client.simulated.start
-  (:require [io.pedestal.app.render.push.handlers.automatic :as d]
+  (:require [io.pedestal.app.protocols :as p]
+            [io.pedestal.app :as app]
+            [io.pedestal.app.render.push.handlers.automatic :as d]
             [tutorial-client.start :as start]
             ;; This needs to be included somewhere in order for the
             ;; tools to work.
-            [io.pedestal.app-tools.tooling :as tooling]))
+            [io.pedestal.app-tools.tooling :as tooling]
+            [tutorial-client.simulated.services :as services]))
 
 (defn ^:export main []
-  (start/create-app d/data-renderer-config))
+  (let [app (start/create-app d/data-renderer-config)
+        services (services/->MockServices (:app app))]
+    (p/start services)
+    app))
