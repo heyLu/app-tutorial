9da17b8352e6c717d32ca9c87f0456f06cb9e035
Brenton Ashworth <brenton@thinkrelevance.com>
2013-06-07 23:12:53 -0400

    add derive functions

--- tutorial-client/app/src/tutorial_client/behavior.clj
+++ tutorial-client/app/src/tutorial_client/behavior.clj
@@ -10,6 +10,24 @@
 (defn swap-transform [_ message]
   (:value message))
 
+(defn average-count [_ inputs]
+  (let [nums (dataflow/input-vals inputs)]
+    (/ (apply + nums) (count nums))))
+
+(defn maximum [old-value inputs]
+  (max (or old-value 0) (or (dataflow/single-val inputs) 0)))
+
+(defn cumulative-average [debug inputs]
+  (let [k (last (first (keys (dataflow/input-map inputs))))
+        x (dataflow/single-val inputs)
+        i (inc (or (::avg-count debug) 0))
+        avg (or (::avg-raw debug) 0)
+        new-avg (+ avg (/ (- x avg) i))]
+    (assoc debug
+      ::avg-count i
+      ::avg-raw new-avg
+      (keyword (str (name k) "-avg")) (int new-avg))))
+
 (defn init-emitter [_]
   [[:transform-enable [:tutorial :my-counter] :inc [{msg/topic [:my-counter]}]]])
 
@@ -20,10 +38,19 @@
 
 (def example-app
   {:version 2
-   :transform [[:inc [:my-counter] inc-transform]
-               [:swap [:other-counters :*] swap-transform]]
+   :debug true
+   :transform [[:inc   [:my-counter]        inc-transform]
+               [:swap  [:other-counters :*] swap-transform]
+               [:debug [:pedestal :**]      swap-transform]]
+   :derive #{[#{[:my-counter] [:other-counters :*]} [:average-count] average-count]
+             [#{[:pedestal :debug :dataflow-time]} [:pedestal :debug :dataflow-time-max] maximum]
+             [#{[:pedestal :debug :dataflow-time]} [:pedestal :debug] cumulative-average]}
    :effect #{[#{[:my-counter]} publish-counter]}
    :emit [{:in #{[:my-counter]
-                 [:other-counters :*]}
+                 [:other-counters :*]
+                 [:average-count]}
            :fn (app/default-emitter :tutorial)
-           :init init-emitter}]})
+           :init init-emitter}
+          [#{[:pedestal :debug :dataflow-time]
+             [:pedestal :debug :dataflow-time-max]
+             [:pedestal :debug :dataflow-time-avg]} (app/default-emitter [])]]})
