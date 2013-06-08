8fbcc376483a6f7b9abaf2fbe031be69a13ee854
Brenton Ashworth <brenton@thinkrelevance.com>
2013-06-08 18:27:25 -0400

    configure the api server

--- tutorial-client/config/config.clj
+++ tutorial-client/config/config.clj
@@ -8,7 +8,8 @@
             :triggers {:html [#"tutorial_client/rendering.js"]}}
     :application {:generated-javascript "generated-js"
                   :default-template "application.html"
-                  :output-root :public}
+                  :output-root :public
+                  :api-server {:host "localhost" :port 8080 :log-fn nil}}
     :control-panel {:design {:uri "/design.html"
                              :name "Design"
                              :order 0}}
@@ -36,6 +37,7 @@
                    :logging? true
                    :output-root :tools-public}
               :development {:uri "/tutorial-client-dev.html"
+                            :use-api-server? true
                             :name "Development"
                             :out-file "tutorial-client-dev.js"
                             :main 'tutorial_client.start
@@ -49,6 +51,7 @@
                       :output-root :tools-public
                       :template "tooling.html"}
               :production {:uri "/tutorial-client.html"
+                           :use-api-server? true
                            :name "Production"
                            :optimizations :advanced
                            :out-file "tutorial-client.js"
