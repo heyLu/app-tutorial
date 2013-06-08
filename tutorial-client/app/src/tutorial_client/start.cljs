b5aef4e5dfa4fa0acc1663297a1a75e8c6dfc2b2
Brenton Ashworth <brenton@thinkrelevance.com>
2013-06-07 23:13:02 -0400

    add post processing

--- tutorial-client/app/src/tutorial_client/start.cljs
+++ tutorial-client/app/src/tutorial_client/start.cljs
@@ -7,8 +7,15 @@
             [tutorial-client.behavior :as behavior]
             [tutorial-client.rendering :as rendering]))
 
+(defn round-number [[op path n]]
+  [[op path (/ (Math.round (* 100 n)) 100)]])
+
+(defn add-post-processors [dataflow]
+  (assoc dataflow
+    :post {:app-model [[:value [:tutorial :average-count] round-number]]}))
+
 (defn create-app [render-config]
-  (let [app (app/build behavior/example-app)
+  (let [app (app/build (add-post-processors behavior/example-app))
         render-fn (push-render/renderer "content" render-config render/log-fn)
         app-model (render/consume-app-model app render-fn)]
     (app/begin app)
