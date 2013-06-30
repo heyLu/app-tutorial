d82774b201dc00d4d82650e69669b650df3fe39d
Brenton Ashworth <brenton@thinkrelevance.com>
2013-06-30 19:17:32 -0400

    bump version numbers

--- tutorial-service/project.clj
+++ tutorial-service/project.clj
@@ -1,4 +1,4 @@
-(defproject tutorial-service "0.1.0-SNAPSHOT"
+(defproject tutorial-service "0.1.4-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
