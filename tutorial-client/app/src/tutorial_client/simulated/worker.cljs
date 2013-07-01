834b069e77079d7488e50a20a098d655c9b7e54d
Brenton Ashworth <brenton@thinkrelevance.com>
2013-07-01 12:05:46 -0400

    start version v2 of tutorial

--- tutorial-client/app/src/tutorial_client/simulated/worker.cljs
+++
@@ -1,5 +0,0 @@
-(ns tutorial-client.simulated.worker
-  (:require [tutorial-client.worker.init :as init]
-            [tutorial-client.simulated.services :as services]))
-
-(init/init! services/->MockServices services/services-fn)
--- tutorial-client/app/src/tutorial_client/worker.cljs
+++
@@ -1,5 +0,0 @@
-(ns tutorial-client.worker
-  (:require [tutorial-client.worker.init :as init]
-            [tutorial-client.services :as services]))
-
-(init/init! services/->Services services/services-fn)
