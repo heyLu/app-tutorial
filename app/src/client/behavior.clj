788bdba125f3c10c4d9ddcd3ae457b1d93e1f7c7
Lucas Stadler <mi.au@papill0n.org>
2013-07-10 23:09:28 +0200

    Add transform for showing other counters.

--- app/src/client/behavior.clj
+++ app/src/client/behavior.clj
@@ -6,6 +6,9 @@
 (defn inc-transform [old-value _]
   ((fnil inc 0) old-value))
 
+(defn swap-transform [_  message]
+  (:value message))
+
 (defn init-main [_]
   ; tells the renderer that a user action related to :my-counter can
   ; send {msg/type :inc msg/topic [:my-counter]} to increment the
@@ -15,7 +18,8 @@
 (def example-app
   {:version 2
               ; type topic (routed to first match in order)
-   :transform [[:inc  [:my-counter] inc-transform]]
+   :transform [[:inc  [:my-counter] inc-transform]
+               [:swap [:**]         swap-transform]]
    :emit [{:init init-main}
-          [#{[:*]} (app/default-emitter [:main])]]})
+          [#{[:my-counter] [:other-counters :*]} (app/default-emitter [:main])]]})
 
