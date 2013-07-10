4446e147ece50951e4fd316b65fc2d12ce17ea5d
Lucas Stadler <mi.au@papill0n.org>
2013-07-10 22:49:43 +0200

    Set up the :inc transform to be renderable.
    So, `transform-enable` tells the renderer that someone could send
    messages and that it would have to rerender?

--- app/src/client/behavior.clj
+++ app/src/client/behavior.clj
@@ -1,12 +1,21 @@
 (ns ^:shared client.behavior
     (:require [clojure.string :as string]
+              [io.pedestal.app :as app]
               [io.pedestal.app.messages :as msg]))
 
 (defn inc-transform [old-value _]
   ((fnil inc 0) old-value))
 
+(defn init-main [_]
+  ; tells the renderer that a user action related to :my-counter can
+  ; send {msg/type :inc msg/topic [:my-counter]} to increment the
+  ; counter
+  [[:transform-enable [:main :my-counter] :inc [{msg/topic [:my-counter]}]]])
+
 (def example-app
   {:version 2
               ; type topic (routed to first match in order)
-   :transform [[:inc [:my-counter] inc-transform]]})
+   :transform [[:inc  [:my-counter] inc-transform]]
+   :emit [{:init init-main}
+          [#{[:*]} (app/default-emitter [:main])]]})
 
